93081aea0d7d2f745512084b55d1fe53d410c635
Howard Good <v-hogood@outlook.com>
2022-10-21 17:26:47 -0700

    Add ViewController.designer.cs

---
+++ TextClassification.iOS/ViewController.designer.cs
@@ -0,0 +1,46 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace TextClassification
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UITableView tableView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UITextField textField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        NSLayoutConstraint textFieldBottomConstraint { get; set; }
+
+        void ReleaseDesignerOutlets()
+        {
+            if (tableView != null) {
+                tableView.Dispose();
+                tableView = null;
+            }
+
+            if (textField != null) {
+                textField.Dispose();
+                textField = null;
+            }
+
+            if (textFieldBottomConstraint != null) {
+                textFieldBottomConstraint.Dispose();
+                textFieldBottomConstraint = null;
+            }
+        }
+    }
+}
